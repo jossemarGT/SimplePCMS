37a369e38a7a761fa55a6ea371308521039e353b
jossemarGT <hello@jossemargt.com>
2014-09-09 10:54:30 -0600

    Initial commit

---
+++ assets/app/app.js
@@ -0,0 +1,34 @@
+(function(ng, _) {
+    'use strict';
+
+    ng.module('SimplePCMS', ['restangular', 'ui.router'])
+        .config(function(RestangularProvider) {
+            RestangularProvider
+                .addRequestInterceptor(function(el, operation) {
+                    if (operation === 'put' || operation === 'post') {
+                        // We don't need to send these to the API
+                        _.each(['createdAt', 'updatedAt', 'definition'], function(prop) {
+                            if (el[prop]) {
+                                delete el[prop];
+                            }
+                        });
+                    }
+                    return el;
+                })
+                .setResponseExtractor(function(response) {
+                    var newResponse = response;
+                    if (_.isArray(response)) {
+                        _.each(newResponse, function(value, idx) {
+                            newResponse[idx].originalElement = _.clone(value, true);
+                        });
+                    } else {
+                        newResponse.originalElement = _.clone(response, true);
+                    }
+                    return newResponse;
+                });
+        });
+
+})(
+    window.angular,
+    window._
+);
